649609e0be5d4029cb69e05186b010ca5925023c
Alisson Santos <alissonamaral182@gmail.com>
2023-05-05 11:03:24 -0300

    12
    12

--- login_verifica.php
+++ login_verifica.php
@@ -1,7 +1,36 @@
 <?php
-    # verifica_login.php
-    session_start();
-    if (!isset($_SESSION['usuario'])) {
-        header('location:login.php?erro=2');
+
+    require('pdo.inc.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+
+    $sql = $pdo->prepare('SELECT * FROM usuario WHERE username = :usr AND ativo = 1');
+    $sql->bindParam(':email', $email);
+
+    $sql->execute();
+
+    // Se encontrou o usuário
+    if ($sql->rowCount()) {
+        // Login feito com sucesso
+        $email = $sql->fetch(PDO::FETCH_OBJ);
+
+        // Verificar se a senha está correta
+        if (!password_verify($senha, $email->senha)) {
+            // Falha no login
+            header('location:login.php?erro=1');
+            die;
+        }
+
+        // Cria uma sessão para armazenar o usuário
+        session_start();
+        $_SESSION['email'] = $email->nome;
+        
+        // Redireciona o usuário
+        header('location:boasvindas.php');
+        die;
+    } else {
+        // Falha no login
+        header('location:login.php?erro=1');
         die;
     }
